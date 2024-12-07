a7828307d28b8751c05af87d8dbf9c0e73603ed9
penghua zhu <zphzhupenghua@outlook.com>
2024-12-08 05:29:16 +0800

    day2完成

--- myCode/client.cpp
+++ myCode/client.cpp
@@ -1,23 +1,51 @@
 /**
  * @file client.cpp
  * @author penghua zhu (zphzhupenghua@outlook.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2024-12-07
- * 
- * 
+ *
+ *
  */
-#include <sys/socket.h>
+#include "util.h"
 #include <arpa/inet.h>
+#include <stdio.h>
 #include <string.h>
-int main(){
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in serv_addr;
-    bzero(&serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serv_addr.sin_port = htons(8883);
-    connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr));
+#include <strings.h>
+#include <sys/socket.h>
+#include <unistd.h>
+int main() {
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in serv_addr;
+  bzero(&serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  serv_addr.sin_port = htons(8883);
+  errif(connect(sockfd, (sockaddr *)&serv_addr, sizeof(serv_addr)) == -1,
+        "socket connect error");
 
-    return 0;
+  while (true) {
+    char buf[1024];
+    bzero(&buf, sizeof(buf));
+    scanf("%s", buf);
+    ssize_t write_bytes = write(sockfd, buf, sizeof(buf));
+
+    if (write_bytes == -1) {
+      printf("socket write error, can't write ant more!\n");
+      break;
+    }
+    bzero(&buf, sizeof(buf));
+    ssize_t read_bytes = read(sockfd, buf, sizeof(buf));
+    if (read_bytes > 0) {
+      printf("message from server: %s\n", buf);
+    } else if (read_bytes == 0) {
+      printf("server socket disconnected!\n");
+      break;
+    } else if (read_bytes == -1) {
+      close(sockfd);
+      errif(true, "socket read error\n");
+    }
+  }
+
+  return 0;
 }
---
+++ myCode/util.h
@@ -0,0 +1,17 @@
+/**
+ * @file util.h
+ * @author penghua zhu (zphzhupenghua@outlook.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-12-08
+ * 
+ * 
+ */
+
+#ifndef UTIL_H
+#define UTIL_H
+
+void errif(bool, const char *);
+
+
+#endif
